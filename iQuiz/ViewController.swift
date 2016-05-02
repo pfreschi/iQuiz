2f8c134a75bd8f70a2ec9251abb05c0fcbb35475
Peter Freschi <pfreschi@uw.edu>
2016-05-01 18:37:58 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Peter Freschi on 5/1/16.
+//  Copyright © 2016 Peter Freschi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
